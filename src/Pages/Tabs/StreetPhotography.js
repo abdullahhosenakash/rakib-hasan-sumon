b93b008a4466678b223adaa459d1c1f08b908628
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-11-05 09:38:34 +0600

    street photography section added

--- src/Pages/Tabs/StreetPhotography.js
+++ src/Pages/Tabs/StreetPhotography.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import './Tabs.css';
 
 const StreetPhotography = () => {
     return (
-        <div>
-            <h2>heloo</h2>
+        <div className='background-color'>
+            <div className="section mx-auto">
+                <h2 className="section-title">S t r e e t &nbsp;  P h o t o g r a p h y</h2>
+                <div className="row row-cols-lg-2 row-cols-1">
+                    <div className="col section-card">
+                        <img className='w-100' src="https://i.ibb.co/P12Xw8q/portfolio2.jpg" alt="" />
+                        <h3 className="section-card-title pt-3">Lorem ipsum dolor sit.</h3>
+                        <p className="section-card-details">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur quia amet officia quibusdam voluptatum ducimus tenetur ratione sit deserunt!
+                        </p>
+                        <button className="section-card-read-more-button">Read More</button>
+                    </div>
+
+                    <div className="col section-card">
+                        <img className='w-100' src="https://i.ibb.co/3cwYSCt/portfolio10.jpg" alt="" />
+                        <h3 className="section-card-title pt-3">Lorem ipsum dolor sit.</h3>
+                        <p className="section-card-details">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur quia amet officia quibusdam voluptatum ducimus tenetur ratione sit deserunt!
+                        </p>
+                        <button className="section-card-read-more-button">Read More</button>
+                    </div>
+                    <div className="col section-card">
+                        <img className='w-100' src="https://i.ibb.co/FYkTz4n/portfolio11.jpg" alt="" />
+                        <h3 className="section-card-title pt-3">Lorem ipsum dolor sit.</h3>
+                        <p className="section-card-details">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur quia amet officia quibusdam voluptatum ducimus tenetur ratione sit deserunt!
+                        </p>
+                        <button className="section-card-read-more-button">Read More</button>
+                    </div>
+                    <div className="col section-card">
+                        <img className='w-100' src="https://i.ibb.co/hcZMnRD/portfolio14.jpg" alt="" />
+                        <h3 className="section-card-title pt-3">Lorem ipsum dolor sit.</h3>
+                        <p className="section-card-details">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur quia amet officia quibusdam voluptatum ducimus tenetur ratione sit deserunt!
+                        </p>
+                        <button className="section-card-read-more-button">Read More</button>
+                    </div>
+                    <div className="col section-card">
+                        <img className='w-100' src="https://i.ibb.co/7p8f4Qn/portfolio18.jpg" alt="" />
+                        <h3 className="section-card-title pt-3">Lorem ipsum dolor sit.</h3>
+                        <p className="section-card-details">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur quia amet officia quibusdam voluptatum ducimus tenetur ratione sit deserunt!
+                        </p>
+                        <button className="section-card-read-more-button">Read More</button>
+                    </div>
+                    <div className="col section-card">
+                        <img className='w-100' src="https://i.ibb.co/vqH6Kzy/portfolio17.jpg" alt="" />
+                        <h3 className="section-card-title pt-3">Lorem ipsum dolor sit.</h3>
+                        <p className="section-card-details">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consectetur quia amet officia quibusdam voluptatum ducimus tenetur ratione sit deserunt!
+                        </p>
+                        <button className="section-card-read-more-button">Read More</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
